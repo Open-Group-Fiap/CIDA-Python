085e983b87663be098e7a5e97277be8d4c2274b0
cauarojas <cauarojas@hotmail.com>
2024-09-11 09:32:58 -0300

    added blob listing

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
-
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
 app = FastAPI()
+
+account_url="cidastore.blob.core.windows.net"
+credential = DefaultAzureCredential()
+
+def get_all_blobs(container_name):
+    blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+    container_client = blob_service_client.get_container_client(container_name)
+    blobs = container_client.list_blobs()
+    return blobs
 
 @app.get("/")
 def read_root():
+    for blob in get_all_blobs("upload-files"):
+        print(blob.name)
     return {"Hello": "World"}
 
 if __name__ == "__main__":
